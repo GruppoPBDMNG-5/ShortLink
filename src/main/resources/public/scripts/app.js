4a77d93d73fa0f75aee93338e40fd6bc50e17532
dmenga <davidemenga92@gmail.com>
2015-08-28 11:39:39 +0200

    Ottimizzato codice controller view "urlStatistics" Eliminata card "Total Clicks" in urlStatistics.html

--- src/main/resources/public/scripts/app.js
+++ src/main/resources/public/scripts/app.js
@@ -72,61 +72,9 @@ app.controller('UrlStatisticsController', function ($scope, $http, $cookieStore)
 
     $http.post('/api/v1/url_statistics', $cookieStore.get("longUrl")).success(function(data) {
 
-        var countryStatistics = data['statistichePaesi'];
-        var chart1 = {};
-        chart1.type = "GeoChart";
-        chart1.data = [
-            ['Country', 'Clicks']
-        ];
-        var item;
-        for (var type in countryStatistics) {
-            item = [];
-            item = type;
-            item = [item, countryStatistics[type]];
-            chart1.data.push(item);
-        }
-        chart1.options = {};
-        $scope.geoChart = chart1;
-        $scope.total_clicks = data['click'];
-
-        var colors = ['red', 'blue', 'grey' , 'orange', 'green', 'purple', 'yellow', 'brown'];
-        var browserStatistics = data['statisticheBrowser'];
-        var chart1 = {};
-        chart1.type = "BarChart";
-        chart1.data = [
-            ['Browser','Clicks',{ role: 'style'}]
-        ];
-        var item;
-        var index = 0;
-        for(var type in browserStatistics) {
-            item = [];
-            item = type;
-            if(index == 7)
-                index = 0;
-            item = [item, browserStatistics[type], colors[index++]];
-            chart1.data.push(item);
-        }
-        chart1.options = {};
-        $scope.browserChart = chart1;
-
-        var platformStatistics = data['statisticheOS'];
-        var chart1 = {};
-        chart1.type = "BarChart";
-        chart1.data = [
-            ['Platform','Clicks',{ role: 'style'}]
-        ];
-        var item;
-        var index = 0;
-        for(var type in platformStatistics) {
-            item = [];
-            item = type;
-            if(index == 7)
-                index = 0;
-            item = [item, platformStatistics[type], colors[index++]];
-            chart1.data.push(item);
-        }
-        chart1.options = {};
-        $scope.platformChart = chart1;
+        $scope.geoChart = geoChart(data['statistichePaesi']);
+        $scope.browserChart = barChart(data['statisticheBrowser'],'Browser', 'Clicks');
+        $scope.platformChart = barChart(data['statisticheOS'],'Platform','Clicks');
 
     });
 
---
+++ src/main/resources/public/scripts/charts.js
@@ -0,0 +1,43 @@
+function geoChart(data) {
+
+    var chart1 = {};
+    chart1.type = "GeoChart";
+    chart1.data = [
+        ['Country', 'Clicks']
+    ];
+    var item;
+    for (var type in data) {
+        item = [];
+        item = type;
+        item = [item, data[type]];
+        chart1.data.push(item);
+    }
+    chart1.options = {};
+
+    return chart1;
+
+}
+
+function barChart(data, key, value) {
+
+    var colors = ['red', 'blue', 'grey' , 'orange', 'green', 'purple', 'yellow', 'brown'];
+    var chart1 = {};
+    chart1.type = "BarChart";
+    chart1.data = [
+        [key,value,{ role: 'style'}]
+    ];
+    var item;
+    var index = 0;
+    for(var type in data) {
+        item = [];
+        item = type;
+        if(index == 7)
+            index = 0;
+        item = [item, data[type], colors[index++]];
+        chart1.data.push(item);
+    }
+    chart1.options = {};
+    return chart1;
+
+}
+
